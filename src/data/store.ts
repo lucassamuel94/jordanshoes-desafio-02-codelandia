09a1eb14faaabab4b8858ddb740e17ed6146bfc4
Lucas Samuel <lucassamuel.dev@gmail.com>
2023-12-06 16:19:50 -0300

    create data store

---
+++ src/data/store.ts
@@ -0,0 +1,52 @@
+export type Store = {
+  id: string,
+  category: string,
+  name: string,
+  imageUrl: string,
+  price: number
+}
+
+export const store : Array<Store> = [
+  {
+    id: '553409d2-9e3b-4e16-a4ed-6cb29da0c5a7',
+    category: 'Tênis Air Jordan',
+    name: 'Air Jordan 1 Mid Dutch Green',
+    imageUrl: '1.png',
+    price: 1199.99
+  },
+  {
+    id: 'edf769df-43b5-4020-9769-27c7e1c58371',
+    category: 'Tênis Air Jordan',
+    name: 'Air Jordan 1 High Zoom CMFT Tropical Twist',
+    imageUrl: '2.png',
+    price: 1049
+  },
+  {
+    id: '23c46baa-812a-43b9-bb98-5be70277dd1c',
+    category: 'Tênis Air Jordan',
+    name: 'Air Jordan 1 Mid Dutch Green',
+    imageUrl: '3.png',
+    price: 1350
+  },
+  {
+    id: '36ef98df-12ca-4fd7-b429-0f42f704b79b',
+    category: 'Tênis Air Jordan',
+    name: 'Air Jordan 1 Mid GS "Light Smoke Grey"',
+    imageUrl: '4.png',
+    price: 1585
+  },
+  {
+    id: '484ba180-c458-4cf5-b7a3-25e58986f9bd',
+    category: 'Tênis Air Jordan',
+    name: 'Air Jordan 1 Mid SE Bright Citrus',
+    imageUrl: '5.png',
+    price: 949.99
+  },
+  {
+    id: 'c6636d1c-e887-4446-8328-2cf567892500',
+    category: 'Tênis Air Jordan',
+    name: 'Air Jordan 1 Mid Grey Camo',
+    imageUrl: '6.png',
+    price: 999.99
+  }
+]
