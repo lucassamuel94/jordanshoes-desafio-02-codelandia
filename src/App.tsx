d82568ac9f807f3dfba070a0dc32b0bb254e432e
Lucas Samuel <lucassamuel.dev@gmail.com>
2023-12-07 10:52:29 -0300

    include components Header and Hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import Header from "./components/header";
+import Hero from "./components/hero";
+
 export default function App() {
   return (
-    <div className="container py-4">
-      <h1>Jordan Shoes</h1>
-      <p>Desafio 02 - Codelândia</p>
-    </div>
+    <>
+      <Header />
+      <Hero />
+    </>
   )
 }
