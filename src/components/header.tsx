bb1c5af8b525e0b9923ffd6cb4be55708977eec0
Lucas Samuel <lucassamuel.dev@gmail.com>
2023-12-07 10:50:22 -0300

    create component Header

---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="text-sm md:text-base lg:text-lg text-center py-3 lg:py-5">
+      <span>Frete grátis para todo o Brasil</span>
+    </header>
+  )
+}
